ac91e7701121d18be15788d32dccd5c18c20a395
Nebelskyi <volumua@gmail.com>
2014-05-07 17:20:28 +0300

    test fixes

--- src/com/uia/example/my/LaunchSettings.java
+++ src/com/uia/example/my/LaunchSettings.java
@@ -1,9 +1,11 @@
 package com.uia.example.my;
 
 import android.graphics.Point;
+import android.webkit.WebView;
 
 import com.android.uiautomator.core.UiObject;
 import com.android.uiautomator.core.UiObjectNotFoundException;
+import com.android.uiautomator.core.UiScrollable;
 import com.android.uiautomator.core.UiSelector;
 import com.android.uiautomator.testrunner.UiAutomatorTestCase;
 
@@ -40,14 +42,29 @@ public class LaunchSettings extends UiAutomatorTestCase {
 		
 		UiObject webView = new UiObject(new UiSelector().resourceId("org.mozilla.firefox:id/layer_view"));
         
-		webView.waitForExists(5000);
+		UiObject progressIcon = new UiObject(new UiSelector().resourceIdMatches("org.mozilla.firefox:id/progress"));
+		progressIcon.waitUntilGone(5000);
+		
 		
         Point p1s = new Point(200,300);
-        Point p1e = new Point(150,300);
+        Point p1e = new Point(150,450);
 
         Point p2s = new Point(300,300);
-        Point p2e = new Point(500,600);
+        Point p2e = new Point(500,250);
         
-        webView.performTwoPointerGesture(p1s, p1e, p2s, p2e, 10);
+        webView.performTwoPointerGesture(p1s, p2s , p1e, p2e, 10);
+        
+        UiObject webPageView = new UiObject(new UiSelector().className("android.view.View"));
+        System.out.println("is scrollable " + webPageView.isScrollable());
+        
+        webPageView.swipeUp(25);
+        webPageView.swipeDown(8);
+        webPageView.swipeRight(2);
+        webPageView.swipeLeft(18);
+        
+//        UiScrollable webPage = new UiScrollable(new UiSelector().scrollable(true));	
+//        webPage.scrollForward();
+//        webPage.scrollForward();
+        
 	}
 }
